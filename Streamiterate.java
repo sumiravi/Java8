f3a185e1242bfd1ae9bbab0dcf0ec302d7d6e156
Sumithra.R <ravikannagi9@gmail.com>
2025-10-11 22:04:01 +0530

    Add files via upload

---
+++ Streamiterate.java
@@ -0,0 +1,7 @@
+import java.util.stream.Stream;
+
+public class Streamiterate {
+    public static void main(String[] args){
+        Stream.iterate(1, n->n+2).limit(5).forEach(System.out::println);
+    }
+}
